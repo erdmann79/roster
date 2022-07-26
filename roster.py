ca9149a780ea753832b89d5a72aefbd94c6012b8
Eric W. Erdmann <eric.erdmann79@gmail.com>
2022-07-26 02:45:54 -0400

    my wife told me to give up and go to bed, the link between the dataframe and the object Series is broken because its a net new object.

--- roster.py
+++ roster.py
@@ -1,5 +1,7 @@
 import pandas, openpyxl
 from openpyxl import load_workbook
+from pprint import pprint
+from itertools import islice
 
 
 class Roster(object):
@@ -21,8 +23,21 @@ class Roster(object):
         Args:
             filename: Name of the workbook file.
         """
-        self.workbook = load_workbook(filename)
-        return
+        self.filename = filename
+        self.workbook = None
+        self.roster_sheet = None
+        self.first_name_column = None
+        self.last_name_column = None
+
+    def __enter__(self):
+        """Open the workbook."""
+        self.workbook = load_workbook(self.filename)
+        self.roster_sheet = self.workbook["Roster"]
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Close the workbook."""
+        self.workbook.close()
 
     def get_student_names(self):
         """
@@ -32,7 +47,16 @@ class Roster(object):
             List of student names
 
         """
-        return self.workbook.get_student_names().list()
+        roster_sheet = self.workbook["Roster"]
+        student_name_list = []
+        for (first, last) in roster_sheet.iter_rows(
+            min_row=2, min_col=2, max_col=3, max_row=1000, values_only=True
+        ):
+            if not first and not last:
+                break
+            student_name_list.append(first + " " + last)
+
+        return student_name_list
 
     def get_student(self, name):
         """
@@ -45,7 +69,59 @@ class Roster(object):
             This is a description of what is returned.
 
         """
-        return self.workbook.get_student(name).str()
+        if name in self.get_student_names():
+            student = {}
+            assingment_results = []
+        for (id_number, first, last) in self.roster_sheet.iter_rows(
+            min_row=2, min_col=1, max_col=3, values_only=True
+        ):
+            if not first and not last:
+                break
+            if name == first + " " + last:
+                student["id"] = id_number
+                break
+        for i, student_sheet in enumerate(self.workbook):
+            if i == 0:
+                continue
+            if name == student_sheet["B2"].internal_value:
+                student["student_sheet"] = student_sheet
+                student["student_sheet_dataframe"] = pandas.DataFrame(student_sheet.values)
+                # print('*************0*')
+
+                # df = pandas.read_excel('29.xlsx', engine='openpyxl',index_col=None)
+
+                # data = student_sheet.values
+                # cols = next(data)[1:]
+                # data = list(data)
+                # idx = [r[0] for r in data]
+                # data = (islice(r, 1, None) for r in data)
+
+                # df = pandas.DataFrame(data, index=idx, columns=cols)
+                # df = pandas.DataFrame(student_sheet)
+
+                # my_series = df['Price'].squeeze()
+                # print(my_series)
+
+                # print('**************')
+
+                for (assingment_result,) in student_sheet.iter_rows(min_row=6, min_col=2, max_col=2, values_only=True):
+                    assingment_results.append(assingment_result)
+                student["grades"] = pandas.Series(assingment_results)
+
+
+
+
+                #print(dir(student_sheet.iter_rows))
+
+                #
+                #student["grades"] = pandas.Series(
+                #    student_sheet.iter_rows(
+                #        min_row=6, min_col=1, max_col=2, values_only=True
+                #    )[0]
+                #)
+                print(student["grades"])
+
+        return student
 
     def class_average(self):
         """
@@ -54,8 +130,23 @@ class Roster(object):
         Returns:
             int class average.
 
+        #    self.assertTrue(roster_obj.class_average() == 614.1 / 7)
+
         """
-        return self.workbook.get_class_average().int()
+        student_avg_list = []
+        for i, student_sheet in enumerate(self.workbook):
+            if i == 0:
+                continue
+            assingment_results = []
+            for (assingment_result,) in student_sheet.iter_rows(
+                min_row=6, min_col=2, max_col=2, values_only=True
+            ):
+                assingment_results.append(assingment_result)
+
+            student_avg_list.append(pandas.Series(assingment_results).mean())
+
+        class_average = sum(student_avg_list) / len(student_avg_list)
+        return class_average
 
     def delete_student(self, name):
         """
@@ -63,21 +154,57 @@ class Roster(object):
 
         Args:
             filename: Name of the student.
-
         """
-        self.workbook.get_student_names(name)
-        return
+        print(len(self.get_student_names()))
+        student_sheet = self.get_student(name)["student_sheet"]
+        self.workbook.remove_sheet(student_sheet)
+        student_list = self.get_student_names()
+        shift_register = 2
+        for i, student_name in enumerate(student_list):
+            row = i + shift_register
+            id_number = i + shift_register - 1
+            if student_name == name:
+                print("remove", name, "row", row, "id", id_number)
+                self.roster_sheet.delete_rows(row)
+                shift_register = 1
+                continue
+            print(row, id_number, student_name)
+            if shift_register == 1:
+                self.roster_sheet[f"A{row}"] = id_number
+                student_sheet = self.get_student(student_name)["student_sheet"]
+                student_sheet["B1"] = id_number
+                student_sheet.title = f"Student_{id_number}"
+                print(student_sheet.title)
+        print(len(self.get_student_names()))
 
-    def save(self, filename):
+    def save(self, filename=None):
         """
         Write workbook to file.
 
         Args:
             filename: Name of the workbook file.
 
-        Returns:
-            This is a description of what is returned.
-
         """
-        self.workbook.save_workbook()
-        return
+        if not filename:
+            filename = self.filename
+        print(filename)
+
+        self.workbook.save(filename)
+
+
+if __name__ == "__main__":
+    with Roster("Jones_2019.xlsx") as roster_obj:
+        student_names = roster_obj.get_student_names()
+        print(student_names)
+        student_record = roster_obj.get_student("Johnny Carson")
+    #    pprint(student_record)
+    #    class_avg = roster_obj.class_average()
+    #    print(class_avg)
+    #    print(614.1 / 7)
+#
+#    roster_obj.delete_student("Allen Dalton")
+#    roster_obj.save("Jones_2019_Reduced.xlsx")
+#    workbook_reduced = load_workbook("Jones_2019_Reduced.xlsx")
+#
+# sheet_names = workbook_reduced.get_sheet_names()
+# print(len(sheet_names))
--- test_roster.py
+++ test_roster.py
@@ -3,53 +3,59 @@ import pandas
 from openpyxl import load_workbook
 from roster import Roster
 
+
 class TestRoster(unittest.TestCase):
+    """Roster slass tests."""
 
     def test_read_roster(self):
-        with Roster("Jones_2019.xlsx") as r:
-            student_names = r.get_student_names()
+        """Test read roster_obj."""
+        with Roster("Jones_2019.xlsx") as roster_obj:
+            student_names = roster_obj.get_student_names()
             self.assertTrue(len(student_names) == 7)
             self.assertTrue("Robert Waters" in student_names)
 
-            catherine = r.get_student("Catherine Hitchens")
+            catherine = roster_obj.get_student("Catherine Hitchens")
             self.assertTrue(catherine["id"] == 3)
             self.assertTrue(isinstance(catherine["grades"], pandas.Series))
             self.assertTrue(len(catherine["grades"]) == 10)
             self.assertTrue(catherine["grades"][4] == 86)
 
-            self.assertTrue(r.class_average() == 614.1/7)
+            self.assertTrue(roster_obj.class_average() == 614.1 / 7)
 
     def test_write_roster(self):
-        with Roster("Jones_2019.xlsx") as r:
-            john = r.get_student("Johnny Carson")
+        """Test write roster_obj."""
+        with Roster("Jones_2019.xlsx") as roster_obj:
+            john = roster_obj.get_student("Johnny Carson")
             for assignment, grade in [(3, 90), (6, 94), (9, 92)]:
                 john["grades"][assignment] = grade
-            self.assertTrue(r.class_average() == 616.6/7)
-            r.save("Jones_2019_Updated.xlsx")
+            self.assertTrue(roster_obj.class_average() == 616.6 / 7)
+            roster_obj.save("Jones_2019_Updated.xlsx")
 
-        wb = load_workbook("Jones_2019_Updated.xlsx")
-        self.assertTrue(wb.get_sheet_by_name("Student_1")["B12"].value == 94)
-        wb.close()
+        workbook = load_workbook("Jones_2019_Updated.xlsx")
+        self.assertTrue(workbook.get_sheet_by_name("Student_1")["B12"].value == 94)
+        workbook.close()
 
     def test_delete_roster_student(self):
+        """Test delete roster student."""
         student_count = 0
-        with Roster("Jones_2019.xlsx") as r:
-            student_count = len(r.get_student_names())
+        with Roster("Jones_2019.xlsx") as roster_obj:
+            student_count = len(roster_obj.get_student_names())
             self.assertTrue(student_count == 7)
-            self.assertTrue(r.get_student("William Thomas")["id"] == 5)
-            r.delete_student("Allen Dalton")
-            student_count = len(r.get_student_names())
+            self.assertTrue(roster_obj.get_student("William Thomas")["id"] == 5)
+            roster_obj.delete_student("Allen Dalton")
+            student_count = len(roster_obj.get_student_names())
             self.assertTrue(student_count == 6)
-            self.assertTrue(r.get_student("William Thomas")["id"] == 4)
-            r.save("Jones_2019_Reduced.xlsx")
+            self.assertTrue(roster_obj.get_student("William Thomas")["id"] == 4)
+            roster_obj.save("Jones_2019_Reduced.xlsx")
 
-        wb = load_workbook("Jones_2019_Reduced.xlsx")
-        sheet_names = wb.get_sheet_names()
+        workbook = load_workbook("Jones_2019_Reduced.xlsx")
+        sheet_names = workbook.sheetnames
         self.assertTrue(len(sheet_names) == 7)
         self.assertTrue(sheet_names[0] == "Roster")
         self.assertTrue(sheet_names[-1] == "Student_6")
-        self.assertTrue(wb.get_sheet_by_name("Student_3")["B7"].value == 92)
-        wb.close()
+        self.assertTrue(workbook["Student_3"]["B7"].value == 92)
+        workbook.close()
+
 
 if __name__ == "__main__":
     unittest.main()
